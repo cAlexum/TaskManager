47dbbb2149541f402985344ca2bfb5e5c1055404
cAlexum <153202415+cAlexum@users.noreply.github.com>
2024-02-01 13:36:21 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ let count = 0;
 
 let taskList = [];
 
-function addTaskTo() {
-    
+function check(someValue) {
+    someValue != ""
 }
+
+function addTaskTo() {
+    alert("Task inputted.")
+};
 function checkAndSend() {
-    if (document.querySelectorAll(".task-form").every(el != "") == true) {
+    if (document.querySelectorAll(".task-form").every(check) == false) {
         alert("Please fill out all of the fields.")
     }
     else {addTaskTo()}
